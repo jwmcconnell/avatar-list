978c12abf58191141c305b615830176dccdecb3e
Jackmc <jackmc1122@gmail.com>
2019-05-28 16:10:42 -0700

    Make CharacterList component and add some basic styling with static data.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import Component from './Component.js';
 import Header from './Header.js';
+import CharacterList from './CharacterList.js';
 
 class App extends Component {
   render() {
@@ -7,7 +8,12 @@ class App extends Component {
 
     const header = new Header();
 
+    const characterList = new CharacterList();
+
+    const main = dom.querySelector('main');
+
     dom.prepend(header.render());
+    main.appendChild(characterList.render());
 
     return dom;
   }
