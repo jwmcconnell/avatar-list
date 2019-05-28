81151eb80f737f5eaa7046d52ee678f8a5e1ef84
Jackmc <jackmc1122@gmail.com>
2019-05-28 15:50:32 -0700

    Add index.js and connect App.js to the html page.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Component from './Component.js';
+
+class App extends Component {
+  renderTemplate() {
+    return /*html*/ `
+      <div>
+        <main>App</main>
+      </div>
+    `;
+  }
+}
+
+export default App;
