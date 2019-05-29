c94eb886e035d91aa4ed1a9c8c5474729cf26950
Jackmc <jackmc1122@gmail.com>
2019-05-29 15:22:36 -0700

    Add rest of images to page.

--- src/components/NationPicker.js
+++ src/components/NationPicker.js
@@ -4,7 +4,10 @@ class NationPicker extends Component {
   renderTemplate() {
     return /*html*/ `
     <section class="nation-picker">
-      <button><img src="../../assets/air-nomads-emblem.png"></button>
+      <button class="air"><img src="../../assets/air-nomads-emblem.png"></button>
+      <button calss="water"><img src="../../assets/water-tribe-emblem.png"></button>
+      <button class="earth"><img src="../../assets/earth-kingdom-emblem.png"></button>
+      <button class="fire"><img src="../../assets/fire-nation-emblem.png"></button>
     </section>
     `;
   }
