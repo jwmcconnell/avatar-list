3990d639cb37f66b51d3b8201687efb4a86e6ac7
Jackmc <jackmc1122@gmail.com>
2019-05-29 14:40:25 -0700

    Remove console.log and use updated url string.

--- src/services/api.js
+++ src/services/api.js
@@ -3,14 +3,15 @@ const URL = 'https://last-airbender-api.herokuapp.com/api/v1/characters';
 const api = {
   getCharacters(type, character) {
     let url = URL;
+
     if(type && character) {
       const searchParams = new URLSearchParams();
       searchParams.set(type, character);
       const query = searchParams.toString();
       url += `?${query}`;
     }
-    console.log(`${url}`);
-    return fetch(`${URL}?${type}=${character}`)
+
+    return fetch(`${url}`)
       .then(res => res.json());
   }
 };
