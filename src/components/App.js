88f19fa6c9118a96e30b2d942eb09e45505ca7ac
Jackmc <jackmc1122@gmail.com>
2019-05-29 13:58:13 -0700

    Move api call into a function and call it everytime the hash changes.

--- src/components/App.js
+++ src/components/App.js
@@ -13,21 +13,30 @@ class App extends Component {
 
     const characterList = new CharacterList({ characters: [] });
 
-    const loading = new Loading({ loading: true });
-
-    api.getCharacters()
-      .then(characters => {
-        characterList.update({ characters });
-      })
-      .finally(() => {
-        loading.update({ loading: false });
-      });
+    const loading = new Loading({ loading: false });
 
     const main = dom.querySelector('main');
 
     dom.prepend(header.render());
     main.appendChild(loading.render());
     main.appendChild(characterList.render());
+
+    function loadCharacters() {
+      loading.update({ loading: true });
+      api.getCharacters()
+        .then(characters => {
+          characterList.update({ characters });
+        })
+        .finally(() => {
+          loading.update({ loading: false });
+        });
+    }
+
+    loadCharacters();
+
+    window.addEventListener('hashchange', () => {
+      loadCharacters();
+    });
 
     return dom;
   }
